271a86ead07a04a75eb162b716760e16e13b0c55
Rhajiit <ksd9638@gmail.com>
2024-04-14 14:18:18 +0900

    ✨ 간단한 input 컴포넌트 구조 생성

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -3,7 +3,13 @@ import React from "react";
 type Props = {};
 
 const Input = (props: Props) => {
-  return <div>input 컴포넌트</div>;
+  return (
+    <div>
+      <label className=" mb-2 text-black"></label>
+      <input type="text" />
+      <p className="ml-2 mt-2 text-red-400">테스트 에러 메시지</p>
+    </div>
+  );
 };
 
 export default Input;
