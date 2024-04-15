e1c90a68ac1dc5e911bc1f47c9fe526438f17718
seoyeon039 <ysynop7@naver.com>
2024-04-15 17:37:13 +0900

    ♻️ Table을 구성하는 데이터를 props로 받을 수 있도록 수정

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -2,47 +2,25 @@ import * as React from 'react';
 import TableColumn from './TableColumn';
 import TableHeader from './TableHeader';
 
-type Props = {};
+interface shopData {
+  id: number;
+  title: string;
+  date: string;
+  cost: number;
+  status: boolean;
+}
 
-const EXAMPLE= [
-  {
-    id: 1,
-    title: 'HS 과일주스',
-    date: '2023.04.15',
-    cost: 15000,
-    status: true,
-  },
-  {
-    id: 2,
-    title: '너구리네 라면집',
-    date: '2023.04.12',
-    cost: 12000,
-    status: true,
-  },
-  {
-    id: 3,
-    title: '써니 레스토랑',
-    date: '2023.04.16',
-    cost: 21000,
-    status: false,
-  },
-  {
-    id: 4,
-    title: '초가을집',
-    date: '2023.04.19',
-    cost: 18000,
-    status: false,
-  },
-]
+type Props = {
+  headerData: string[];
+  data: shopData[]
+};
 
-const HEADER_COL_NAME = ['가게', '날짜', '시급', '상태']
-
-const Table = (props:Props) => {
+const Table = ({ headerData, data }: Props) => {
   return (
     <>
-      <div className='grid grid-cols-1 w-4/6 m-4 border border-gray-20 rounded-lg'>
-        <TableHeader headerData={HEADER_COL_NAME}/>
-        <TableColumn data={EXAMPLE}/>
+      <div className='grid grid-cols-1 m-4 border border-gray-20 rounded-lg'>
+        <TableHeader headerData={headerData}/>
+        <TableColumn data={data}/>
       </div>
     </>
   );
