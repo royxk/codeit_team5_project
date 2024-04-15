8ed6c5a1f5aae887d3e3c75ba2d2ff922eff89f4
Rhajiit <ksd9638@gmail.com>
2024-04-15 14:44:54 +0900

    ✨ 별도의 상수 파일에서 필요한 문자열을 불러오도록 함

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,15 +1,26 @@
 "use client";
+import {
+  INPUT_ERROR_TYPE,
+  INPUT_LABELS,
+  INPUT_LAST_WORD,
+} from "@/util/constants/INPUT_VALUES";
 import React from "react";
 
 interface InputProps {
   inputType?: string;
+  errorType?: string;
+  blurEvent?: () => void;
 }
 
-const Input = ({ inputType = "default" }: InputProps) => {
+const Input = ({
+  inputType = "default",
+  errorType = "",
+  blurEvent = () => console.log("blured"),
+}: InputProps) => {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 relative">
       <label className="text-black" htmlFor={inputType}>
-        테스트 라벨
+        {INPUT_LABELS[inputType]}
       </label>
       <label
         className="relative flex justify-between px-5 py-4 border-[1px] border-gray-30 focus-within:border-blue-20 z-[1] cursor-text bg-white"
@@ -19,11 +30,15 @@ const Input = ({ inputType = "default" }: InputProps) => {
           id={inputType}
           className="w-full focus-visible:outline-none rounded-md"
           type="text"
-          onBlur={() => console.log("blurd")}
+          onBlur={blurEvent}
         />
-        <p>{inputType}</p>
+        {INPUT_LAST_WORD[inputType] && (
+          <p className="text-nowrap">{INPUT_LAST_WORD[inputType]}</p>
+        )}
       </label>
-      <p className="ml-2 text-red-400">에러 메시지</p>
+      {INPUT_ERROR_TYPE[errorType] && (
+        <p className="ml-2 text-red-400">{INPUT_ERROR_TYPE[errorType]}</p>
+      )}
     </div>
   );
 };
---
+++ src/util/constants/INPUT_VALUES.ts
@@ -0,0 +1,15 @@
+interface InputValuesType {
+  [key: string]: string;
+}
+
+export const INPUT_LABELS: InputValuesType = {
+  default: "기본 라벨",
+};
+
+export const INPUT_LAST_WORD: InputValuesType = {
+  default: "기본 어미",
+};
+
+export const INPUT_ERROR_TYPE: InputValuesType = {
+  default: "지정되지 않은 오류",
+};
