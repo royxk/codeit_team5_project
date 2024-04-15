2e24c2d64900012f8377fc9cb9e0011b00f0d0bb
Rhajiit <ksd9638@gmail.com>
2024-04-15 17:03:53 +0900

    📝 input의 타입에 따른 상수형 문자열 추가

--- src/util/constants/INPUT_VALUES.ts
+++ src/util/constants/INPUT_VALUES.ts
@@ -2,14 +2,42 @@ interface InputValuesType {
   [key: string]: string;
 }
 
+export const INPUT_TYPES: InputValuesType = {
+  email: "email",
+  password: "password",
+  passwordCheck: "password",
+  date: "date",
+  workHour: "number",
+  wage: "number",
+};
+
 export const INPUT_LABELS: InputValuesType = {
   default: "기본 라벨",
+  email: "이메일",
+  password: "비밀번호",
+  passwordCheck: "비밀번호 확인",
+  date: "시작 일시*",
+  types: "분류*",
+  wage: "시급*",
+  workHour: "업무 시간",
+};
+
+export const INPUT_PLACEHOLDER: InputValuesType = {
+  default: "inputType을 지정해 주세요",
+  email: "이메일을 입력해 주세요.",
+  password: "비밀번호를 입력해 주세요.",
+  passwordCheck: "비밀번호를 동일하게 입력해 주세요.",
+  wage: "시급을 입력해 주세요.",
+  workHour: "업무 시간을 입력해 주세요.",
 };
 
 export const INPUT_LAST_WORD: InputValuesType = {
   default: "기본 어미",
+  wage: "원",
 };
 
 export const INPUT_ERROR_TYPE: InputValuesType = {
-  default: "지정되지 않은 오류",
+  default: "inputType을 지정해주세요.",
 };
+
+export const INPUT_SELECT_TYPE: string[] = ["types"];
