061f9de4d189a5f5fff419b0db3e6ddda9f6eccb
seoyeon039 <ysynop7@naver.com>
2024-04-16 17:33:09 +0900

    ✨ [사장님] 응답으로 받은 데이터를 Table 컴포넌트에서 사용할 수 있도록 재구성하는 함수 작성

--- src/util/convertData.ts
+++ src/util/convertData.ts
@@ -1,4 +1,5 @@
-//응답 데이터 필요한 타입 정의
+// 응답 데이터 필요한 타입 정의
+// 알바님(일반회원) 공고 지원 목록 데이터 타입
 export type ApplyShopApiResponse = {
   items:
     {
@@ -21,13 +22,43 @@ export type ApplyShopApiResponse = {
     }[];
   };
 
-export interface ConvertApplyShopData {
+// 사장님(가게) 지원자 목록 데이터 타입
+export type ApplyEmployeeApiResponse = {
+  items:
+    {
+      item: {
+        status: string;
+        user: {
+          item: {
+            id: string;
+            name?: string; // optional
+            phone?: string; // optional
+            bio?: string; // optional
+          };
+        };
+      };
+    }[];
+  };
+
+// 새로운 데이터 타입 정의
+interface CommonData {
+  apply_id: string;
+  status: string;
+}
+
+export interface ConvertApplyShopData extends CommonData {
   apply_id: string;
   shopName: string;
   hourlyPay: number;
   startsAt: string;
   workHour: number;
   status: string;
+};
+
+export interface ConvertApplicantData extends CommonData {
+  userName?: string;
+  phoneNumber?: string;
+  bio?: string;
 };
 
 //Table 컴포넌트에서 사용할 수 있도록 데이터를 변환하는 함수.
@@ -40,6 +71,19 @@ export const convertApplyShopData = (responseData: ApplyShopApiResponse): Conver
       "hourlyPay": notice.item.hourlyPay,
       "startsAt": notice.item.startsAt,
       "workHour": notice.item.workhour,
+      "status": status,
+    };
+  });
+};
+
+export const convertApplicantData = (responseData: ApplyEmployeeApiResponse): ConvertApplicantData[] => {
+  return responseData.items.map((data) => {
+    const { user, status } = data.item;
+    return {
+      "apply_id": user.item.id,
+      "userName": user.item.name,
+      "phoneNumber": user.item.phone,
+      "bio": user.item.bio,
       "status": status,
     };
   });
