02d5ba5a3bfa20e721ad8b0f5f0ec0137f3e55c1
Rhajiit <ksd9638@gmail.com>
2024-04-16 15:19:51 +0900

    💄 현재 선택된 페이지에 따른 state변화 적용

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 interface PaginationProps {
   totalNumberOfItems: number;
@@ -7,19 +7,23 @@ interface PaginationProps {
 const pageNum = [1, 2, 3, 4, 5, 6, 7];
 
 const Pagination = ({ totalNumberOfItems }: PaginationProps) => {
+  const [currentPage, setCurrentPage] = useState(0);
+
   return (
     <div className="flex h-16 w-full items-center justify-center gap-5 bg-white p-3 mob:h-[3.625rem]">
       <button
         type="button"
-        className="h-full max-h-5 w-full max-w-5 bg-[url('/pagination-left-possible.svg')]"
+        className={`h-full max-h-5 w-full max-w-5 
+        ${currentPage == 0 ? "bg-[url('/pagination-left-impossible.svg')]" : "bg-[url('/pagination-left-possible.svg')]"}`}
       />
       <div className="flex gap-1">
-        {pageNum.map((item) => (
+        {pageNum.map((item, i) => (
           <button
-            key={item}
-            className={`h-10 w-10 rounded-[4px] bg-red-30 text-white`}
+            key={i}
+            className={`h-10 w-10 rounded-[4px] ${currentPage === i ? "bg-red-30 text-white" : "text-black"}`}
+            onClick={() => setCurrentPage(i)}
           >
-            {item}
+            {i + 1}
           </button>
         ))}
       </div>
