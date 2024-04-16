8c8b84c594fa9db2933c9c1671099d04ba99286c
Rhajiit <ksd9638@gmail.com>
2024-04-16 15:26:42 +0900

    💄 pagination필요를 판정하여 화살표를 표시하도록 함

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -8,14 +8,17 @@ const pageNum = [1, 2, 3, 4, 5, 6, 7];
 
 const Pagination = ({ totalNumberOfItems }: PaginationProps) => {
   const [currentPage, setCurrentPage] = useState(0);
+  const isPaginationNeed = pageNum.length > 7;
 
   return (
     <div className="flex h-16 w-full items-center justify-center gap-5 bg-white p-3 mob:h-[3.625rem]">
-      <button
-        type="button"
-        className={`h-full max-h-5 w-full max-w-5 
+      {isPaginationNeed && (
+        <button
+          type="button"
+          className={`h-full max-h-5 w-full max-w-5 
         ${currentPage == 0 ? "bg-[url('/pagination-left-impossible.svg')]" : "bg-[url('/pagination-left-possible.svg')]"}`}
-      />
+        />
+      )}
       <div className="flex gap-1">
         {pageNum.map((item, i) => (
           <button
@@ -27,10 +30,12 @@ const Pagination = ({ totalNumberOfItems }: PaginationProps) => {
           </button>
         ))}
       </div>
-      <button
-        type="button"
-        className="h-full max-h-5 w-full max-w-5 bg-[url('/pagination-right.svg')]"
-      />
+      {isPaginationNeed && currentPage !== pageNum.length - 1 && (
+        <button
+          type="button"
+          className="h-full max-h-5 w-full max-w-5 bg-[url('/pagination-right.svg')]"
+        />
+      )}
     </div>
   );
 };
