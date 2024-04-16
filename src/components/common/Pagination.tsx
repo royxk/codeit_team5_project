3cd1aa6bdb660f9899796beeaa16d9fe2790ad45
Rhajiit <ksd9638@gmail.com>
2024-04-16 17:33:15 +0900

    🐛 pageData가 없을 경우 ui가 사리지는 문제 해결

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -7,12 +7,6 @@ interface PaginationProps {
   pageItemLimit?: number;
 }
 
-const testData = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50,
-];
-
 /**
  * @param {pageData[]} rawPageData 페이지에 표시할 순수 배열 데이터를 받는 param입니다. 이 데이터를 가공하여 [페이지][페이지 데이터] 형태의 2차원 배열로 가공합니다.
  * @param {StateSetFunction} setCurrentPageData 현재 페이지에 보여줄 데이터를 설정하는 setState함수를 받아 페이지 선택 시 그 값으로 설정합니다.
@@ -20,7 +14,7 @@ const testData = [
  * @returns
  */
 const Pagination = ({
-  rawPageData = testData,
+  rawPageData = [],
   setCurrentPageData = (item) => {
     console.log(`${item}`);
   },
@@ -32,21 +26,28 @@ const Pagination = ({
 
   // rawPageData 가공
   const tempPageData: unknown[] = [];
-  rawPageData.map((item, index) => {
-    tempPageData.push(item);
-    if (
-      index === rawPageData.length - 1 ||
-      (index !== 0 && (index + 1) % pageItemLimit === 0)
-    ) {
-      pageData.push(tempPageData.slice());
-      tempPageData.splice(0);
-    }
-  });
+  if (rawPageData.length !== 0) {
+    rawPageData.map((item, index) => {
+      tempPageData.push(item);
+      if (
+        index === rawPageData.length - 1 ||
+        (index !== 0 && (index + 1) % pageItemLimit === 0)
+      ) {
+        pageData.push(tempPageData.slice());
+        tempPageData.splice(0);
+      }
+    });
+  }
   const isPaginationNeed = pageData.length > 7;
+
   const firstPageList: number[] = [];
   if (!isPaginationNeed) {
-    for (let i = 0; i < pageData.length; i++) {
-      firstPageList.push(i);
+    if (pageData.length === 0) {
+      firstPageList.push(0);
+    } else {
+      for (let i = 0; i < pageData.length; i++) {
+        firstPageList.push(i);
+      }
     }
   }
 
