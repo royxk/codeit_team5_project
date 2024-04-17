f48efc4e600c617c411c337412a0967877db751e
seoyeon039 <ysynop7@naver.com>
2024-04-17 17:15:58 +0900

    ♻️ 불필요한 함수 제거 및 구조분해 할당 적용

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -17,12 +17,9 @@ interface ApplyData {
   bio?: string;
 }
 
-function showDate(startDate: string | undefined, workHour: number | undefined) {
-  return `${startDate}(${workHour}시간)`
-}
-
 /**
- * @param applyData 지원한 사람/지원한 공고 에 대한 데이터를 받아오는 파라미터입니다. 타입은 임시로 지정해 둔 것으로 이후 api 응답에 따라 차차 바뀔 예정입니다.
+ * @param {string[]} headerData 테이블의 헤더 부분의 데이터를 받아오는 파라미터입니다.
+ * @param {ApplyData} applyData 지원한 사람/지원한 공고 에 대한 데이터를 받아오는 파라미터입니다.
  */
 
 const Table = <T extends ApplyData>({ headerData, applyData }: TableProps<T>) => {
@@ -35,20 +32,31 @@ const Table = <T extends ApplyData>({ headerData, applyData }: TableProps<T>) =>
           <TableHeader headerData={headerData}/>
           <tbody>
             {applyData.map((data) => {
-              const statusLabel = data.status === "pending" ? "대기중"
-                : data.status === "accepted" ? "승인 완료"
-                : data.status === "rejected" ? "거절"
+              const {
+                apply_id,
+                status,
+                shopName, 
+                hourlyPay,
+                startsAt,
+                workHour,
+                userName,
+                phoneNumber,
+                bio
+              } = data;
+              const statusLabel = status === "pending" ? "대기중"
+                : status === "accepted" ? "승인 완료"
+                : status === "rejected" ? "거절"
                 : "취소";
               return (
-                <tr key={data.apply_id} className='border-b border-gray-20'>
+                <tr key={apply_id} className='border-b border-gray-20'>
                   <td className='bg-white px-3 py-5 w-full min-w-[226px] sticky z-10 left-0'>
-                    {isEmployee ? data.shopName : data.userName}
+                    {isEmployee ? shopName : userName}
                   </td>
                   <td className='bg-white px-3 py-5 w-full min-w-[300px]'>
-                    {isEmployee ? showDate(data.startsAt, data.workHour) : data.bio}
+                    {isEmployee ? `${startsAt}(${workHour}시간)` : bio}
                   </td>
                   <td className='bg-white px-3 py-5 w-full min-w-[226px]'>
-                    {isEmployee ? `${data.hourlyPay}원` : data.phoneNumber}
+                    {isEmployee ? `${hourlyPay}원` : phoneNumber}
                   </td>
                   <td className='bg-white px-3 py-5 w-full min-w-[226px]'>
                     {statusLabel}
