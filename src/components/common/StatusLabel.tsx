8ad2260ae7184e24d9f0b4b7dd0b86b8311b55cd
seoyeon039 <ysynop7@naver.com>
2024-04-18 01:48:06 +0900

    ♻️ status를 prop으로 받고 해당 status에 따라 label의 색상이 변경되도록 코드 수정

--- src/components/common/StatusLabel.tsx
+++ src/components/common/StatusLabel.tsx
@@ -1,17 +1,33 @@
 interface StatusLabelProp {
+  status: Status;
 }
 
-const StatusLabel = (props: StatusLabelProp) => {
+type Status = "pending" | "accepted" | "rejected";
+//isEmployee 값을 EmployeeTable/EmployerTable 컴포넌트에서 받아와야겠다.
+
+function StatusCssString(status: Status) {
+  let statusCss;
+  switch(status) {
+    case "pending":
+      statusCss= 'bg-green-10 text-green-20 w-16';
+      break;
+    case "accepted":
+      statusCss= 'bg-blue-10 text-blue-20 w-20';
+      break;
+    default:
+      statusCss= 'bg-red-10 text-red-40 w-12';
+      break;
+  }
+
+  return statusCss;
+}
+
+const StatusLabel = ({ status }: StatusLabelProp) => {
+  const statusCss = StatusCssString(status);
   return (
     <>
-    <div className='bg-green-10 text-green-20 w-16 border-none rounded-[20px] text-center px-[10px] py-[6px] font-bold text-sm'>
-      대기중
-    </div>
-    <div className='bg-blue-10 text-blue-20 w-20 border-none rounded-[20px] text-center px-[10px] py-[6px] font-bold text-sm'>
-      승인 완료
-    </div>
-    <div className='bg-red-10 text-red-40 w-12 border-none rounded-[20px] text-center px-[10px] py-[6px] font-bold text-sm'>
-      거절
+    <div className={`${statusCss} border-none rounded-[20px] text-center px-[10px] py-[6px] font-bold text-sm`}>
+      상태
     </div>
     <div className='flex gap-3'>
       <button type='button' className='flex items-center bg-white text-red-40 border border-red-40 rounded-md px-5 h-[38px] py-[10px] font-bold text-sm hover:bg-red-10'>
