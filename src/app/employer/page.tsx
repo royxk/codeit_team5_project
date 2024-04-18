f6755e14bbb9ef65e6810f977ebb7561d27c1370
Rhajiit <ksd9638@gmail.com>
2024-04-18 14:24:33 +0900

    💄 employer 페이지 기본 구조 생성

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -3,7 +3,16 @@ import React from "react";
 type Props = {};
 
 const Employer = (props: Props) => {
-  return <h1>가게 정보 상세</h1>;
+  return (
+    <div className="mx-auto min-h-[calc(100vh-18rem)] max-w-[64.25rem] px-8 py-[3.75rem]">
+      <main className="">
+        <h1 className="h1">내 가게</h1>
+        <section className="rounded-xl border-[1px] border-gray-20 p-6">
+          asdfasdf
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Employer;
