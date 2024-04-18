40bf5738886ed0777ca7982dbf1cf4fe2d4c5296
Rhajiit <ksd9638@gmail.com>
2024-04-18 17:59:32 +0900

    ♻️ 공고에만 있을 것으로 예상되는 dueTime 유무에 따라 컴포넌트가 변동되도록 함

--- src/app/storeDetailTest/page.tsx
+++ src/app/storeDetailTest/page.tsx
@@ -1,4 +1,3 @@
-import Footer from "@/components/common/Footer";
 import StoreDetail from "@/components/common/StoreDetail";
 import React from "react";
 
@@ -7,7 +6,7 @@ type Props = {};
 const StoreDetailPageTest = (props: Props) => {
   return (
     <div className="p-8">
-      <StoreDetail isEmployerMainPage={false} />
+      <StoreDetail />
     </div>
   );
 };
--- src/components/common/StoreDetail/index.tsx
+++ src/components/common/StoreDetail/index.tsx
@@ -6,7 +6,7 @@ interface StoreDetailProps {
   isEmployerMainPage?: boolean;
 }
 
-const data = {
+const sampleData = {
   name: "도토리 식당",
   dueTime: "2023-01-02 15:00~18:00 (3시간)",
   mainAddress: "서울시 송파구",
@@ -14,15 +14,14 @@ const data = {
     "알바하기 편한 너구리네 라면집!  라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다. ㅁㄴㅇㄹㅇㄴㅁㄹㅇㄴㄹㅇㄴㄹㅇㄴㄹㅇㄹㅇㅁㄴㄹㅁㅇㄴㄹㅇㄴㄹㅇㄴㄹㅇㄴㅁㄹㅇㄴㅁㄹㅁㅇㄴㄹㅁㅇㄴ ㄹㅇㄴㄹㅁㅇㄴ ㄹㅁㅇㄴㄹㅁㅇㄴㄹㅇㄴㄹㅁㅇㄴㅇㄹㅇㄴㄹㅁㅇㄴㄹㅁㅇㄴ ㄹㅇㄴㅁ ㄹㅇㄴㄹ ㅇㄴㅁ ㄹㅁㅇㄴ ㄹㅁㅇㄴ ㄹ",
 };
 
-const StoreDetail = ({
-  isEmployerMainPage = false,
-  isUserEmployer = false,
-}) => {
+const StoreDetail = ({ data = sampleData }) => {
+  const isPostPage = "dueTime" in data;
+
   return (
     <main
       className={`flex h-[22.25rem] w-full max-w-[60.25rem] flex-row gap-x-8 overflow-hidden rounded-xl border-[1px] border-gray-20 p-6
                   tab:h-auto tab:flex-col mob:p-5
-      ${isEmployerMainPage ? "bg-red-10" : "bg-white"}`}
+      ${!isPostPage ? "bg-red-10" : "bg-white"}`}
     >
       <div className="relative h-full w-full  overflow-hidden rounded-xl tab:h-[20.5625rem] mob:max-h-[11.0625rem]">
         <Image
@@ -38,10 +37,12 @@ const StoreDetail = ({
       >
         <div className="flex flex-col gap-3">
           <div>
-            <h1 className="body1-bold mob:body2-bold text-primary">시급</h1>
+            <h1 className="body1-bold mob:body2-bold text-primary">
+              {isPostPage ? "시급" : "가게"}
+            </h1>
             <h2 className="h1 mob:h2 mt-2">{data.name}</h2>
           </div>
-          {data.dueTime && (
+          {isPostPage && (
             <p className="body1 mob:body2 text-gray-50">{data.dueTime}</p>
           )}
           <p className="body1 mob:body2 text-gray-50">{data.mainAddress}</p>
