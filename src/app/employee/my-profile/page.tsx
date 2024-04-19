e55aa01595877e5d4ee8772ca422f931424fbd7e
seoyeon039 <ysynop7@naver.com>
2024-04-19 23:09:50 +0900

    ✨ 내 프로필 상세 페이지 기본 UI 구현

---
+++ src/app/employee/my-profile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const componentName = () => {
+  return (
+    <div>
+      안녕 내 프로필
+    </div>
+  );
+};
+
+export default componentName;
--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import Button from "@/components/common/Button";
+import Link from "next/link";
 
 type Props = {};
 
 const Employee = (props: Props) => {
-  return <h1>프로필 상세</h1>;
+  return( 
+    <>
+      <h1>프로필 navbar</h1>
+      <header className="flex flex-col justify-start items-center w-full mb-[459px] tab:mb-[568px]">
+        <div className="flex flex-col gap-6 py-[60px]">
+          <h1 className="h1">내 프로필</h1>
+          <div>
+            <div className="flex flex-col items-center gap-6 w-[964px] border border-gray-20 rounded-lg px-6 py-[60px] tab:w-[632px] mob:text-sm mob:w-[350px]">
+              <p className="text-black">내 프로필을 등록하고 원하는 가게에 지원해 보세요</p>
+              <Link href="/employee/my-profile">
+                <Button size="large" color="red" className="mob:px-5 py-[10px] text-sm">내 프로필 등록하기</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Employee;
