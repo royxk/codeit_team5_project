425f377226522bab31f1ae46a046c680a6176b42
Rhajiit <ksd9638@gmail.com>
2024-04-20 19:41:57 +0900

    ✅ 생성한 공고 리스트를 확인하는 테스트 페이지 생성

---
+++ src/app/employer/postTest/page.tsx
@@ -0,0 +1,14 @@
+import PostEmployer from "@/components/employer/PostEmployer";
+import React from "react";
+
+type Props = {};
+
+const EmployerPostTest = (props: Props) => {
+  return (
+    <div className="max-w-[964px]">
+      <PostEmployer />
+    </div>
+  );
+};
+
+export default EmployerPostTest;
