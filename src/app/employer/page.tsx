51a7c34e457644cc85b819e90ac5ac4d55ac79ee
Rhajiit <ksd9638@gmail.com>
2024-04-20 18:07:36 +0900

    💄 가가게 상세 컴포넌트 작성으로 인해 불필요해진 일부 요소 삭제

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -4,14 +4,7 @@ type Props = {};
 
 const Employer = (props: Props) => {
   return (
-    <div className="mx-auto min-h-[calc(100vh-18rem)] max-w-[64.25rem] px-8 py-[3.75rem]">
-      <main className="">
-        <h1 className="h1">내 가게</h1>
-        <section className="rounded-xl border-[1px] border-gray-20 p-6">
-          asdfasdf
-        </section>
-      </main>
-    </div>
+    <div className="mx-auto min-h-[calc(100vh-18rem)] max-w-[64.25rem] px-8 py-[3.75rem]"></div>
   );
 };
 
