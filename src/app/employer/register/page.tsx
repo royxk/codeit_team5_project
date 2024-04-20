036befe7f2a25eccacbd9c7cb13e7453dc74e4fe
Rhajiit <ksd9638@gmail.com>
2024-04-20 18:25:59 +0900

    💄 본격적인 작어베 앞서 간단한 레이아웃 구성

---
+++ src/app/employer/register/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const EmployerRegisterStore = (props: Props) => {
+  return (
+    <main className="m-auto min-h-[calc(100vh-10.625rem)] max-w-[64.3125rem] px-8 py-[3.75rem]">
+      assign
+    </main>
+  );
+};
+
+export default EmployerRegisterStore;
