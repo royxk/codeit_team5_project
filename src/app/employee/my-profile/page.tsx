0de92c41244f3e0a4fa22915ac5363f6880a021d
seoyeon039 <ysynop7@naver.com>
2024-04-21 01:22:07 +0900

    💄 프로필 등록 페이지 반응형 디자인 추가

--- src/app/employee/my-profile/page.tsx
+++ src/app/employee/my-profile/page.tsx
@@ -14,14 +14,14 @@ const RegisterProfile = () => {
 
   return (
     <>
-      <div className='flex flex-col items-center gap-8 py-[60px] bg-gray-5'>
-        <div className='flex justify-between w-[964px]'>
-          <h1 className='h1'>내 프로필</h1>
+      <div className='flex flex-col items-center gap-8 py-[60px] bg-gray-5 tab:pb-[278px] mob:pb-20'>
+        <div className='flex justify-between w-[964px] tab:w-[632px] mob:w-[350px]'>
+          <h1 className='h1 mob:h3'>내 프로필</h1>
           <button type='button' onClick={handleClick}>
-            <Image src={closeIcon} alt='closeBtn'/>
+            <Image src={closeIcon} className='w-8 mob:w-6' alt='closeBtn'/>
           </button>
         </div>
-        <form className='grid grid-cols-3 gap-5 w-[964px]'>
+        <form className='grid grid-cols-3 gap-5 w-[964px] tab:grid-cols-2 tab:w-[632px] mob:grid-cols-1 mob:w-[350px]'>
           <div>
             <Input inputType="NAME"/>
           </div>
@@ -31,7 +31,7 @@ const RegisterProfile = () => {
           <div>
             <Input inputType={INPUT_SELECT_TYPE[2]} dataArray={INPUT_SELECT_DATA_LIST.MAIN_ADDRESS} />
           </div>
-          <div className='col-span-3 flex flex-col gap-2'>
+          <div className='col-span-3 flex flex-col gap-2 tab:col-span-2 mob:col-span-1'>
             <label>소개</label>
             <textarea
               className='w-full border border-gray-30 rounded-lg h-40 px-5 py-4 focus:outline-none focus:border-blue-20'
