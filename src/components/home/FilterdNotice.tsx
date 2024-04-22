fe1391c1a4ba6f78630b92995e4c510bfce7ee12
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-22 17:18:49 +0900

    ✨ 필터링된 전체공고 수정

--- src/components/home/FilterdNotice.tsx
+++ src/components/home/FilterdNotice.tsx
@@ -1,30 +1,71 @@
-import { NoticeResponse } from "@/app/page";
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Post from "../common/Post/Post";
 import { formatApiDateData } from "@/util/formatDate";
+import { NoticeResponse } from "@/app/page";
+import { Query, searchNoticeApiResponse } from "@/util/api";
+import PostSkeleton from "../common/Post/PostSkeleton";
+import Pagination from "../common/Pagination";
 
-type Props = { res: NoticeResponse };
+const FilterdNotice = () => {
+  const [filterdNoticeList, setFilterdNoticeList] = useState<NoticeResponse>();
 
-const FilterdNotice = ({ res }: Props) => {
+  const getFilterdNoticeData = async (query?: Query) => {
+    const res = await searchNoticeApiResponse(query);
+    console.log(res);
+    setFilterdNoticeList(res);
+  };
+  const handlePageData = async (num: number) => {
+    const res = await searchNoticeApiResponse({
+      offset: num ? (num - 1) * 6 : 0,
+      limit: 6,
+      // 설정된 상세필터 적용
+    });
+    console.log(res);
+    setFilterdNoticeList(res);
+  };
+  useEffect(() => {
+    getFilterdNoticeData();
+  }, []);
+
+  if (!filterdNoticeList)
+    return (
+      <>
+        {
+          <div className="flex w-full snap-x items-start gap-[14px] overflow-x-scroll hide-scrollbar">
+            {[...Array(6)].map((_, index) => (
+              <PostSkeleton key={index} />
+            ))}
+          </div>
+        }
+      </>
+    );
+
   return (
-    <div className="grid w-full grid-cols-3 gap-x-[14px] gap-y-[31px] tab:grid-cols-2 mob:auto-rows-auto mob:gap-2">
-      {res?.items.map((item, index) => (
-        <Post
-          key={index}
-          imgUrl={item.item.shop.item.imageUrl}
-          shopName={item.item.shop.item.name}
-          address1={item.item.shop.item.address1}
-          hourlyPay={item.item.hourlyPay}
-          startTime={
-            formatApiDateData(item.item.startsAt, item.item.workhour)[0]
-          }
-          startHour={
-            formatApiDateData(item.item.startsAt, item.item.workhour)[1]
-          }
-          state={!item.item.closed}
-        />
-      ))}
-    </div>
+    <>
+      <div className="grid w-full grid-cols-3 gap-x-[14px] gap-y-[31px] tab:grid-cols-2 mob:auto-rows-auto mob:gap-2">
+        {filterdNoticeList?.items.map((item, index) => (
+          <Post
+            key={index}
+            imgUrl={item.item.shop.item.imageUrl}
+            shopName={item.item.shop.item.name}
+            address1={item.item.shop.item.address1}
+            hourlyPay={item.item.hourlyPay}
+            startTime={
+              formatApiDateData(item.item.startsAt, item.item.workhour)[0]
+            }
+            startHour={
+              formatApiDateData(item.item.startsAt, item.item.workhour)[1]
+            }
+            state={!item.item.closed}
+          />
+        ))}
+      </div>
+      <Pagination
+        rawPageData={filterdNoticeList.items}
+        setCurrentPageData={handlePageData}
+      />
+    </>
   );
 };
 
