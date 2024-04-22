7f4aef7fa3922a49a98dd1bf138016b3704a90f4
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-22 15:41:04 +0900

    ✨ 메인 페이지 ui 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,61 @@ import CustomNotice from "@/components/home/CustomNotice";
 import Pagination from "@/components/common/Pagination";
 import { searchNoticeApiResponse } from "@/util/api";
 import { useEffect, useState } from "react";
+import Post from "@/components/common/Post/Post";
+import S from "@/util/constants/TEST_POST_VALUES";
+import { formatApiDateData } from "@/util/formatDate";
+
+export interface NoticeResponse {
+  offset: number;
+  limit: number;
+  address: string[];
+  count: number;
+  hasNext: boolean;
+  items: NoticeItem[];
+  links: Link[];
+}
+
+interface NoticeItem {
+  item: {
+    id: string;
+    hourlyPay: number;
+    startsAt: string;
+    workhour: number;
+    description: string;
+    closed: boolean;
+    shop: {
+      item: {
+        id: string;
+        name: string;
+        category: string;
+        address1: string;
+        address2: string;
+        description: string;
+        imageUrl: string;
+        originalHourlyPay: number;
+      };
+      href: string;
+    };
+  };
+  links: Link[];
+}
+
+interface Link {
+  rel: string;
+  description: string;
+  method: string;
+  href: string;
+}
 
 export default function Home() {
-  const [customNoticeList, setCustomNoticeList] = useState();
-  const customNoticeData = async () => {
-    const { items } = await searchNoticeApiResponse();
-    console.log(items);
-    setCustomNoticeList(items);
+  const [customNoticeList, setCustomNoticeList] = useState<NoticeResponse>();
+  const getCustomNoticeData = async () => {
+    const res = await searchNoticeApiResponse();
+    console.log(res);
+    setCustomNoticeList(res);
   };
   useEffect(() => {
-    customNoticeData();
+    getCustomNoticeData();
   }, []);
   return (
     <>
@@ -23,7 +68,7 @@ export default function Home() {
           <h1 className="w-full pb-[31px] text-left text-[28px] font-bold tracking-[0.56px]">
             맞춤공고
           </h1>
-          {/* <CustomNotice items={customNoticeList} /> */}
+          {customNoticeList && <CustomNotice res={customNoticeList} />}
         </div>
       </section>
       <section className="flex justify-center py-[60px]">
@@ -35,9 +80,31 @@ export default function Home() {
               <div>상세 필터</div>
             </div>
           </div>
-          {/* <div className="grid w-full grid-cols-3 gap-x-[14px] gap-y-[31px]"></div> */}
-          <PostTest />
-          {/* <Pagination rawPageData={} setCurrentPageData={} /> */}
+          <div className="grid w-full grid-cols-3 gap-x-[14px] gap-y-[31px] tab:grid-cols-2 mob:auto-rows-auto mob:gap-2">
+            {S.items.map((item, index) => (
+              <Post
+                key={index}
+                imgUrl={item.item.shop.item.imageUrl}
+                shopName={item.item.shop.item.name}
+                address1={item.item.shop.item.address1}
+                hourlyPay={item.item.hourlyPay}
+                startTime={
+                  formatApiDateData(item.item.startsAt, item.item.workhour)[0]
+                }
+                startHour={
+                  formatApiDateData(item.item.startsAt, item.item.workhour)[1]
+                }
+                state={!item.item.closed}
+              />
+            ))}
+          </div>
+
+          {customNoticeList && (
+            <Pagination
+              rawPageData={customNoticeList.items}
+              setCurrentPageData={}
+            />
+          )}
         </div>
       </section>
     </>
