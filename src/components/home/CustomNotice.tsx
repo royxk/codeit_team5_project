c029ac7da4bcbff0d924f002c1430d1bebad4140
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-22 17:10:48 +0900

    ✨ 맞춤공고 컴포넌트 수정

--- src/components/home/CustomNotice.tsx
+++ src/components/home/CustomNotice.tsx
@@ -1,15 +1,27 @@
 "use client";
-import S from "@/util/constants/TEST_POST_VALUES";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Post from "../common/Post/Post";
 import { formatApiDateData } from "@/util/formatDate";
 import { NoticeResponse } from "@/app/page";
-// import { formatDate } from "@/app/postTest/page";
+import { Query, searchNoticeApiResponse } from "@/util/api";
+import PostSkeleton from "../common/Post/PostSkeleton";
 
-type Props = { res: NoticeResponse };
-
-const CustomNotice = ({ res }: Props) => {
+const CustomNotice = () => {
+  const [customNoticeList, setCustomNoticeList] = useState<NoticeResponse>();
   const scrollRef = useRef<HTMLDivElement>(null);
+
+  const getCustomNoticeData = async () => {
+    const res = await searchNoticeApiResponse({
+      // address: "유저의 주소 사용",
+    });
+    console.log(res);
+    setCustomNoticeList(res);
+  };
+
+  useEffect(() => {
+    getCustomNoticeData();
+  }, []);
+
   useEffect(() => {
     const interval = setInterval(() => {
       if (scrollRef.current) {
@@ -35,29 +47,40 @@ const CustomNotice = ({ res }: Props) => {
     return () => clearInterval(interval);
   }, []);
 
+  if (!customNoticeList)
+    return (
+      <>
+        {
+          <div className="flex w-full snap-x items-start gap-[14px] overflow-x-scroll hide-scrollbar">
+            {[...Array(3)].map((_, index) => (
+              <PostSkeleton key={index} />
+            ))}
+          </div>
+        }
+      </>
+    );
+
   return (
     <div
       ref={scrollRef}
       className="flex w-full snap-x items-start gap-[14px] overflow-x-scroll hide-scrollbar"
     >
-      {res?.items
-        .slice(0, 6)
-        .map((item, index) => (
-          <Post
-            key={index}
-            imgUrl={item.item.shop.item.imageUrl}
-            shopName={item.item.shop.item.name}
-            address1={item.item.shop.item.address1}
-            hourlyPay={item.item.hourlyPay}
-            startTime={
-              formatApiDateData(item.item.startsAt, item.item.workhour)[0]
-            }
-            startHour={
-              formatApiDateData(item.item.startsAt, item.item.workhour)[1]
-            }
-            state={!item.item.closed}
-          />
-        ))}
+      {customNoticeList?.items.map((item) => (
+        <Post
+          key={item.item.id}
+          imgUrl={item.item.shop.item.imageUrl}
+          shopName={item.item.shop.item.name}
+          address1={item.item.shop.item.address1}
+          hourlyPay={item.item.hourlyPay}
+          startTime={
+            formatApiDateData(item.item.startsAt, item.item.workhour)[0]
+          }
+          startHour={
+            formatApiDateData(item.item.startsAt, item.item.workhour)[1]
+          }
+          state={!item.item.closed}
+        />
+      ))}
     </div>
   );
 };
