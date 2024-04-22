5e92dc424ed574d6fd2a75aa0e613d8df831a144
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-22 17:20:08 +0900

    ✨ 필터를 포함하는 공고검색을 컴포넌트화

---
+++ src/components/home/NoticeMain.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BasicFilter from "./BasicFilter";
+import AdvancedFilterComponent from "../common/AdvancedFilter/AdvancedFilterComponent";
+import FilterdNotice from "./FilterdNotice";
+
+type Props = {};
+
+const NoticeMain = (props: Props) => {
+  return (
+    <section className="flex justify-center py-[60px]">
+      <div className="flex max-w-[964px] flex-col items-center">
+        <div className="flex w-full justify-between pb-[31px] text-left text-[28px] font-bold tracking-[0.56px]">
+          <h1>전체공고</h1>
+          <div className="flex h-[42px] items-center gap-[10px]">
+            <BasicFilter />
+            <AdvancedFilterComponent />
+          </div>
+        </div>
+        <FilterdNotice />
+      </div>
+    </section>
+  );
+};
+
+export default NoticeMain;
