8ef966766a8df2f4cc79bf32763ba1f6775d647c
Rhajiit <ksd9638@gmail.com>
2024-04-22 17:27:26 +0900

    ➕ employer 페이지에 StoreDetail컴포넌트 적용

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -1,10 +1,13 @@
+import StoreDetail from "@/components/common/StoreDetail";
 import React from "react";
 
 type Props = {};
 
 const Employer = (props: Props) => {
   return (
-    <div className="mx-auto min-h-[calc(100vh-18rem)] max-w-[64.25rem] px-8 py-[3.75rem]"></div>
+    <div className="mx-auto min-h-[calc(100vh-18rem)] max-w-[64.25rem] px-8 py-[3.75rem]">
+      <StoreDetail data={} />
+    </div>
   );
 };
 
