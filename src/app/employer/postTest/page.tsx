ccf1939e51b6c6dc58ac2a7ff933fe6828136bad
Rhajiit <ksd9638@gmail.com>
2024-04-22 17:58:24 +0900

    💄 기초 ui 생성 전 mock데이터 적용

--- src/app/employer/postTest/page.tsx
+++ src/app/employer/postTest/page.tsx
@@ -1,12 +1,14 @@
 import PostEmployer from "@/components/employer/PostEmployer";
+import { STORE_DETAIL_EMPLOYER } from "@/util/constants/STORE_DETAIL_EMPLOYER";
 import React from "react";
 
 type Props = {};
 
 const EmployerPostTest = (props: Props) => {
+  const data = STORE_DETAIL_EMPLOYER;
   return (
     <div className="max-w-[964px]">
-      <PostEmployer />
+      <PostEmployer data={data} />
     </div>
   );
 };
--- src/components/employer/PostEmployer.tsx
+++ src/components/employer/PostEmployer.tsx
@@ -16,18 +16,36 @@ interface PostDataType {
 // startTime: string;
 // startHour: string;
 
-const PostEmployer = (props: Props) => {
-  const data = EMPLOYER_POST_LIST;
+function formatDate(startsAt: string, workhour: number): string[] {
+  const start = new Date(startsAt);
+  const end = new Date(start.getTime() + workhour * 3600000);
+
+  // Ensuring month and day are two digits
+  const formattedMonth = (start.getMonth() + 1).toString().padStart(2, "0");
+  const formattedDay = start.getDate().toString().padStart(2, "0");
+  const formattedStartHours = start.getHours().toString().padStart(2, "0");
+  const formattedStartMinutes = start.getMinutes().toString().padStart(2, "0");
+  const formattedEndHours = end.getHours().toString().padStart(2, "0");
+  const formattedEndMinutes = end.getMinutes().toString().padStart(2, "0");
+
+  return [
+    `${start.getFullYear()}-${formattedMonth}-${formattedDay}`,
+    `${formattedStartHours}:${formattedStartMinutes}~${formattedEndHours}:${formattedEndMinutes} (${workhour}시간)`,
+  ];
+}
+
+const PostEmployer = ({ data }: any) => {
+  const data2 = EMPLOYER_POST_LIST;
 
   function DataConvertComponentStandard(PostData: PostDataType) {
     return {
-      imgUrl: "imgUrl",
-      shopName: "shopName",
-      address1: "address1",
+      imgUrl: data.item.imageUrl,
+      shopName: data.item.name,
+      address1: data.item.address1,
       hourlyPay: PostData.hourlyPay,
       state: PostData.closed,
-      startTime: "asdf",
-      startHour: "asdf",
+      startTime: formatDate(PostData.startsAt, PostData.workhour)[0],
+      startHour: formatDate(PostData.startsAt, PostData.workhour)[1],
     };
   }
 
@@ -35,7 +53,7 @@ const PostEmployer = (props: Props) => {
     <div>
       <p className="h1 text-block mb-8">내가 등록한 공고</p>
       <div className="grid grid-cols-3 gap-x-[0.875rem] gap-y-8">
-        {data.items.map((item) =>
+        {data2.items.map((item) =>
           Post(DataConvertComponentStandard(item.item)),
         )}
       </div>
