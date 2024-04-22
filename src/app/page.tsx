302afb7804b1b69fed92d720af196396da795deb
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-22 13:56:43 +0900

    ✨ 메인 페이지 ui 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
 import BasicFilter from "@/components/home/BasicFilter";
+import PostTest from "./postTest/page";
+import CustomNotice from "@/components/home/CustomNotice";
+import Pagination from "@/components/common/Pagination";
+import { searchNoticeApiResponse } from "@/util/api";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [customNoticeList, setCustomNoticeList] = useState();
+  const customNoticeData = async () => {
+    const { items } = await searchNoticeApiResponse();
+    console.log(items);
+    setCustomNoticeList(items);
+  };
+  useEffect(() => {
+    customNoticeData();
+  }, []);
   return (
     <>
       <section className="flex justify-center bg-red-10 py-[60px]">
@@ -8,11 +23,7 @@ export default function Home() {
           <h1 className="w-full pb-[31px] text-left text-[28px] font-bold tracking-[0.56px]">
             맞춤공고
           </h1>
-          <div className="inline-flex items-start gap-[14px]">
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-          </div>
+          {/* <CustomNotice items={customNoticeList} /> */}
         </div>
       </section>
       <section className="flex justify-center py-[60px]">
@@ -24,14 +35,8 @@ export default function Home() {
               <div>상세 필터</div>
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-x-[14px] gap-y-[31px]">
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-            <div className="h-[349px] w-[312px] bg-gray-50">카드 컴포넌트</div>
-          </div>
+          <PostTest />
+          {/* <Pagination rawPageData={} setCurrentPageData={} /> */}
         </div>
       </section>
     </>
