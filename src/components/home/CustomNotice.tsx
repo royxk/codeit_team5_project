3eb91373a7e1464d6146a1402edd2940649a36a1
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-22 15:39:51 +0900

    ✨ 실제 데이터 맞춤공고에 적용

--- src/components/home/CustomNotice.tsx
+++ src/components/home/CustomNotice.tsx
@@ -2,11 +2,13 @@
 import S from "@/util/constants/TEST_POST_VALUES";
 import React, { useEffect, useRef } from "react";
 import Post from "../common/Post/Post";
+import { formatApiDateData } from "@/util/formatDate";
+import { NoticeResponse } from "@/app/page";
 // import { formatDate } from "@/app/postTest/page";
 
-type Props = {};
+type Props = { res: NoticeResponse };
 
-const CustomNotice = ({ items }: { items: [] }) => {
+const CustomNotice = ({ res }: Props) => {
   const scrollRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const interval = setInterval(() => {
@@ -38,38 +40,26 @@ const CustomNotice = ({ items }: { items: [] }) => {
       ref={scrollRef}
       className="flex w-full snap-x items-start gap-[14px] overflow-x-scroll hide-scrollbar"
     >
-      {S.items.slice(0, 6).map((item, index) => (
-        <Post
-          key={index}
-          imgUrl={item.item.shop.item.imageUrl}
-          shopName={item.item.shop.item.name}
-          address1={item.item.shop.item.address1}
-          hourlyPay={item.item.hourlyPay}
-          startTime={formatDate(item.item.startsAt, item.item.workhour)[0]}
-          startHour={formatDate(item.item.startsAt, item.item.workhour)[1]}
-          state={!item.item.closed}
-        />
-      ))}
+      {res?.items
+        .slice(0, 6)
+        .map((item, index) => (
+          <Post
+            key={index}
+            imgUrl={item.item.shop.item.imageUrl}
+            shopName={item.item.shop.item.name}
+            address1={item.item.shop.item.address1}
+            hourlyPay={item.item.hourlyPay}
+            startTime={
+              formatApiDateData(item.item.startsAt, item.item.workhour)[0]
+            }
+            startHour={
+              formatApiDateData(item.item.startsAt, item.item.workhour)[1]
+            }
+            state={!item.item.closed}
+          />
+        ))}
     </div>
   );
 };
-
-function formatDate(startsAt: string, workhour: number): string[] {
-  const start = new Date(startsAt);
-  const end = new Date(start.getTime() + workhour * 3600000);
-
-  // Ensuring month and day are two digits
-  const formattedMonth = (start.getMonth() + 1).toString().padStart(2, "0");
-  const formattedDay = start.getDate().toString().padStart(2, "0");
-  const formattedStartHours = start.getHours().toString().padStart(2, "0");
-  const formattedStartMinutes = start.getMinutes().toString().padStart(2, "0");
-  const formattedEndHours = end.getHours().toString().padStart(2, "0");
-  const formattedEndMinutes = end.getMinutes().toString().padStart(2, "0");
-
-  return [
-    `${start.getFullYear()}-${formattedMonth}-${formattedDay}`,
-    `${formattedStartHours}:${formattedStartMinutes}~${formattedEndHours}:${formattedEndMinutes} (${workhour}시간)`,
-  ];
-}
 
 export default CustomNotice;
