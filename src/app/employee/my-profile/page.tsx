213bf9082d7ce413d004e42440ea541a6f6221bf
seoyeon039 <ysynop7@naver.com>
2024-04-23 23:59:46 +0900

    ✨ input 값을 가져와 제출시 리퀘스트를 보낼 객체 생성 & 이전 프로필 데이터 input 안에 남아있게 구현

--- src/app/employee/my-profile/page.tsx
+++ src/app/employee/my-profile/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useEffect, useRef, useState } from 'react';
 import { INPUT_SELECT_DATA_LIST, INPUT_SELECT_TYPE } from '@/util/constants/INPUT_VALUES';
 import { useRouter } from 'next/navigation';
 import Button from '@/components/common/Button';
@@ -7,11 +8,46 @@ import Image from 'next/image';
 import closeIcon from '/public/close.svg';
 import { USER_TEST_DATA } from '@/util/constants/PROFILE_PAGE_USER_TEST_DATA';
 
+//처음 회원가입 후 내 정보 조회하면 shop은 null이 나오지만, name, phone, address 등등의 값은 없는 채로 리스폰스가 온다.
+
 const RegisterProfile = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const phoneNumRef = useRef<HTMLInputElement>(null);
+  const [addressValue, setAddressValue] = useState('');
+  const bioRef = useRef<HTMLTextAreaElement>(null);
   const router = useRouter();
   const isProfileData = Object.keys(USER_TEST_DATA.item).length <= 3;
+
+  useEffect(() => {
+    const { item } = USER_TEST_DATA;
+    if (nameRef.current && item.name) {
+      nameRef.current.value = item.name;
+    }
+    if (phoneNumRef.current && item.phone) {
+      phoneNumRef.current.value = item.phone;
+    }
+    if (bioRef.current && item.bio) {
+      bioRef.current.value = item.bio;
+    }
+  }, [])
+
   const handleClick = () => {
     router.push('/employee');
+  }
+
+  const handleSelect = (data: string) => {
+    setAddressValue(data);
+  }
+
+  const handleSubmit = () => {
+    const editValue = {
+      name: nameRef.current?.value,
+      phone: phoneNumRef.current?.value,
+      address: addressValue,
+      bio: bioRef.current?.value,
+    }
+
+    console.log(editValue)
   }
 
   return (
@@ -25,23 +61,28 @@ const RegisterProfile = () => {
         </div>
         <form className='grid grid-cols-3 gap-5 w-[964px] tab:grid-cols-2 tab:w-[632px] mob:grid-cols-1 mob:w-[350px]'>
           <div>
-            <Input inputType="NAME"/>
+            <Input inputType="NAME" inputRef={nameRef}/>
           </div>
           <div>
-            <Input inputType="PHONE_NUMBER"/>
+            <Input inputType="PHONE_NUMBER" inputRef={phoneNumRef}/>
           </div>
           <div>
-            <Input inputType={INPUT_SELECT_TYPE[2]} dataArray={INPUT_SELECT_DATA_LIST.MAIN_ADDRESS} />
+            <Input
+              inputType={INPUT_SELECT_TYPE[2]}
+              dataArray={INPUT_SELECT_DATA_LIST.MAIN_ADDRESS}
+              selectData={handleSelect}
+            />
           </div>
           <div className='col-span-3 flex flex-col gap-2 tab:col-span-2 mob:col-span-1'>
             <label>소개</label>
             <textarea
               className='w-full border border-gray-30 rounded-lg h-40 px-5 py-4 focus:outline-none focus:border-blue-20'
               placeholder='자기 소개를 입력해 주세요.'
+              ref={bioRef}
             />
           </div>
         </form>
-        <Button size='large' color='red'>{isProfileData ? '등록하기' : '수정하기'}</Button>
+        <Button size='large' color='red' onClick={handleSubmit}>{isProfileData ? '등록하기' : '수정하기'}</Button>
       </div>
     </>
   );
