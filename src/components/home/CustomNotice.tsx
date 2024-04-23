3977eab33df824a427e4d865873d7811845be219
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-23 19:16:49 +0900

    ✨ 화면 크기에 따라 달라지는 post의 width에 대응하여 자동으로 우측 스크롤 되는 값을 조정하면서 resize 또한 debounce를 추가하여 너무 많은 resize함수의 호출을 방지하는 코드 추가

--- src/components/home/CustomNotice.tsx
+++ src/components/home/CustomNotice.tsx
@@ -6,10 +6,27 @@ import { NoticeResponse } from "@/app/page";
 import { searchNoticeApiResponse } from "@/util/api";
 import PostSkeleton from "../common/Post/PostSkeleton";
 
+function debounce<T extends (...args: any[]) => any>(func: T, wait: number): T {
+  let timeout: NodeJS.Timeout | null = null;
+
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
+    const later = () => {
+      timeout = null;
+      func.apply(this, args);
+    };
+
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(later, wait);
+  } as T;
+}
+
 const CustomNotice = () => {
   const [customNoticeList, setCustomNoticeList] = useState<NoticeResponse>();
   const scrollRef = useRef<HTMLDivElement>(null);
-
+  const [scrollDistance, setScrollDistance] = useState<number>(326);
   const getCustomNoticeData = async () => {
     const res = await searchNoticeApiResponse({
       // address: "유저의 주소 사용",
@@ -23,10 +40,30 @@ const CustomNotice = () => {
   }, []);
 
   useEffect(() => {
+    // 화면 너비에 따라 스크롤 이동 거리를 결정하는 함수
+    const checkSizeAndScroll = () => {
+      const newScrollDistance = window.innerWidth < 768 ? 178 : 326;
+      console.log(`New scroll distance: ${newScrollDistance}px`);
+      setScrollDistance(newScrollDistance);
+    };
+
+    // debounce 적용
+    const debouncedCheckSize = debounce(checkSizeAndScroll, 250);
+
+    // 이벤트 리스너 추가
+    window.addEventListener("resize", debouncedCheckSize);
+
+    // 초기 스크롤 이동 거리 설정
+    checkSizeAndScroll();
+
+    // 10초마다 스크롤 이동
     const interval = setInterval(() => {
       if (scrollRef.current) {
         const currentScrollPosition = scrollRef.current.scrollLeft;
-        const newScrollPosition = currentScrollPosition + 312;
+        console.log(`Current scroll position: ${currentScrollPosition}px`);
+
+        const newScrollPosition = currentScrollPosition + scrollDistance;
+        console.log(`Scrolling to: ${newScrollPosition}px`);
 
         scrollRef.current.scrollTo({
           left: newScrollPosition,
@@ -35,23 +72,28 @@ const CustomNotice = () => {
 
         setTimeout(() => {
           if (scrollRef?.current?.scrollLeft === currentScrollPosition) {
+            console.log("Reached the end, scrolling back to start.");
             scrollRef.current.scrollTo({
               left: 0,
               behavior: "smooth",
             });
           }
-        }, 500);
+        }, 1000);
       }
     }, 10000);
 
-    return () => clearInterval(interval);
-  }, []);
+    // 컴포넌트 언마운트 시 이벤트 리스너와 인터벌 정리
+    return () => {
+      window.removeEventListener("resize", debouncedCheckSize);
+      clearInterval(interval);
+    };
+  }, [scrollDistance]);
 
   if (!customNoticeList)
     return (
       <>
         {
-          <div className="flex w-full snap-x items-start gap-[14px] overflow-x-scroll hide-scrollbar">
+          <div className="flex w-full items-start gap-[14px] overflow-x-scroll hide-scrollbar mob:gap-2">
             {[...Array(5)].map((_, index) => (
               <PostSkeleton key={index} />
             ))}
@@ -63,7 +105,7 @@ const CustomNotice = () => {
   return (
     <div
       ref={scrollRef}
-      className="flex snap-x justify-start gap-[14px] overflow-x-scroll hide-scrollbar"
+      className="flex w-full justify-start gap-[14px] overflow-x-scroll hide-scrollbar mob:gap-2"
     >
       {customNoticeList?.items.map((item) => (
         <Post
