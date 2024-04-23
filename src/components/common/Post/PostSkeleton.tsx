4bedb0a8a36b247a7b534c83118a4b60c952a0a6
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-04-23 14:07:09 +0900

    🎨 Fix Skeleton UI fixed

--- src/components/common/Post/PostSkeleton.tsx
+++ src/components/common/Post/PostSkeleton.tsx
@@ -11,8 +11,8 @@ const PostSkeleton = () => {
           <div className="flex w-full flex-row items-center gap-4 mob:gap-4">
             <div className="h-4 w-4 rounded-full bg-gray-300"></div>{" "}
             <div className="flex flex-row gap-4 mob:flex-col mob:gap-1">
-              <div className="h-3 w-20 rounded bg-gray-300 mob:w-20"></div>
-              <div className="h-3 w-20 rounded bg-gray-300 mob:w-full"></div>{" "}
+              <div className="h-4 w-20 rounded bg-gray-300 mob:w-20"></div>
+              <div className="h-4 w-20 rounded bg-gray-300 mob:w-full"></div>{" "}
             </div>
           </div>
 
@@ -23,8 +23,8 @@ const PostSkeleton = () => {
         </div>
 
         <div className="flex w-full flex-row items-center justify-between gap-4 mob:flex-col mob:items-start mob:gap-1">
-          <div className="h-[30px] w-1/3 rounded bg-gray-300 mob:h-[20px] mob:w-full"></div>
-          <div className="flex h-[40px] w-[145px] items-center rounded-3xl bg-gray-300 mob:h-[20px] mob:w-full mob:rounded"></div>
+          <div className="h-[30px] w-[110px] rounded bg-gray-300 mob:h-[15px] mob:w-full"></div>
+          <div className="flex h-[40px] w-[150px] items-center rounded-3xl bg-gray-300 mob:h-[15px] mob:w-full mob:rounded"></div>
         </div>
       </div>
     </div>
