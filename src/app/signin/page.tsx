340a0d0515f208c2d5d4adc610468a2a130bffae
GoM <honorrulru@gmail.com>
2024-04-23 13:52:30 +0900

    feat: 로그인페이지 ui구현

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,51 @@
+import Button from "@/components/common/Button";
+import Input from "@/components/common/Input";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-const Signin = (props: Props) => {
-  return <h1>로그인</h1>;
+const Signup = (props: Props) => {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex h-[288px] w-[350px] flex-col">
+        <div className="m-10 flex items-center justify-center">
+          <img
+            src="logoIcon.png"
+            alt="logoIcon"
+            className="h-[45px] w-[248px]"
+          />
+        </div>
+        <div className="mb-5 flex flex-col">
+          <div className="mb-6 flex flex-col">
+            <span className="mb-1">이메일</span>
+            <Input inputType="email"></Input>
+          </div>
+          <div className="flex flex-col">
+            <span className="mb-1">비밀번호</span>
+            <Input inputType="password"></Input>
+          </div>
+        </div>
+        <div className="mb-5 flex h-[48px] w-[350px] items-center justify-center">
+          <Button size="large" color="red">
+            로그인 하기
+          </Button>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="flex items-center justify-center font-Pretendard text-sm font-normal">
+            <span className="mr-3">회원이 아니신가요?</span>{" "}
+            <Link
+              href="/signup"
+              className="text-[#5534Da]"
+              style={{ textDecoration: "underline" }}
+            >
+              회원가입하기
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Signin;
+export default Signup;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,39 +1,9 @@
-import Button from "@/components/common/Button";
-import Input from "@/components/common/Input";
-import Link from "next/link";
 import React from "react";
-import Signin from "../signin/page";
 
 type Props = {};
 
-const Signup = (props: Props) => {
-  return (
-    <div className="flex items-center justify-center">
-      <div className="flex flex-col">
-        <div className="flex h-[45px] w-[248px]">the-julge</div>
-        <div className="flex flex-col">
-          <div className="mb-2 flex flex-col">
-            <span>이메일</span>
-            <Input inputType="email"></Input>
-          </div>
-          <div className="mb-2 flex flex-col">
-            <span>비밀번호</span>
-            <Input inputType="password"></Input>
-          </div>
-        </div>
-        <div className="flex">
-          <Button size="large" color="red" className="m-2">
-            로그인 하기
-          </Button>
-        </div>
-        <div className="flex h-[19px] w-[214px]">
-          <div className="flex">
-            회원이 아니신가요? <span>회원가입하기</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+const signup = (props: Props) => {
+  return <h1>회원가입</h1>;
 };
 
-export default Signup;
+export default signup;
