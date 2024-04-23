1725141262f82311f913339bcc0f2570db068900
Rhajiit <ksd9638@gmail.com>
2024-04-23 17:49:04 +0900

    ✨ 사장 index페이지에서 쿠키를 받아 데이터를 불러올 수 있도록 함

--- src/app/employer/page.tsx
+++ src/app/employer/page.tsx
@@ -1,12 +1,47 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import StoreDetail from "@/components/common/StoreDetail";
-import React from "react";
+import {
+  mydataApiResponse,
+  searchShopInformationApiResponse,
+} from "@/util/api";
+import { getCookie, setShopIdCookie } from "@/util/cookieSetting";
+import { useRouter } from "next/navigation";
 
-type Props = {};
+const Employer = () => {
+  const [storeData, setStoreData] = useState({ data: { item: undefined } });
+  const router = useRouter();
 
-const Employer = (props: Props) => {
+  async function userStoreLoad(uid: string) {
+    const { item } = await mydataApiResponse(uid);
+    const sid = getCookie("sid");
+
+    if (!sid && item.type === "employer" && item.shop) {
+      const { id: shopId } = item.shop.item;
+      setShopIdCookie(shopId);
+      const shopData = await searchShopInformationApiResponse(shopId);
+      setStoreData(shopData);
+      return;
+    }
+
+    if (sid) {
+      const shopData = await searchShopInformationApiResponse(sid);
+      setStoreData(shopData);
+    }
+  }
+
+  useEffect(() => {
+    const uid = getCookie("uid");
+    if (uid !== undefined) {
+      userStoreLoad(uid);
+    } else {
+      router.push("/signin");
+    }
+  }, []);
+
   return (
-    <div className="mx-auto min-h-[calc(100vh-18rem)] max-w-[64.25rem] px-8 py-[3.75rem]">
-      <StoreDetail data={} />
+    <div className="mx-auto min-h-[calc(100vh-10.625rem)] max-w-[64.25rem] px-8 py-[3.75rem]">
+      <StoreDetail data={storeData} />
     </div>
   );
 };
