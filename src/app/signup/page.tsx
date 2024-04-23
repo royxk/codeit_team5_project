4504c0c945eecaf6d89e31a334416d30a8e0c267
GoM <honorrulru@gmail.com>
2024-04-23 14:32:04 +0900

    feat: 회원가입 페이지 ui 구현

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,80 @@
+import Button from "@/components/common/Button";
+import Input from "@/components/common/Input";
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
-const signup = (props: Props) => {
-  return <h1>회원가입</h1>;
+const Signin = (props: Props) => {
+  return (
+    <div className="flex h-screen items-center justify-center pb-[360px]">
+      <div className="flex h-[288px] w-[350px] flex-col">
+        <div className="m-10 flex items-center justify-center">
+          <img
+            src="signup/logoIcon.png"
+            alt="logoIcon"
+            className="h-[45px] w-[248px]"
+          />
+        </div>
+        <div className="mb-5 flex flex-col">
+          <div className="mb-6 flex flex-col">
+            <span className="mb-1">이메일</span>
+            <Input inputType="email"></Input>
+          </div>
+          <div className="mb-6 flex flex-col">
+            <span className="mb-1">비밀번호</span>
+            <Input inputType="password"></Input>
+          </div>
+          <div className="flex flex-col">
+            <span className="mb-1">비밀번호 확인</span>
+            <Input inputType="password"></Input>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <span className="mb-1">회원 유형</span>
+          <div className="mb-6  flex justify-between gap-[8px]">
+            <button
+              type="button"
+              className="w-[167px]items-center flex h-[50px] rounded-[30px] border border-gray-30 px-[41px] py-[13px]"
+            >
+              <img
+                src="signup/grayCircleIcon.png"
+                alt="circleIcon"
+                className="relative left-[-4px] top-[1px] mr-2 h-5 w-5"
+              />
+              알바님
+            </button>
+            <button
+              type="button"
+              className="w-[167px]items-center flex h-[50px] rounded-[30px] border border-gray-30 px-[41px] py-[13px]"
+            >
+              <img
+                src="signup/grayCircleIcon.png"
+                alt="circleIcon"
+                className="relative left-[-4px] top-[1px] mr-2 h-5 w-5"
+              />
+              사장님
+            </button>
+          </div>
+        </div>
+        <div className="mb-5 flex h-[48px] w-[350px] items-center justify-center">
+          <Button size="large" color="red">
+            로그인 하기
+          </Button>
+        </div>
+        <div className="flex items-center justify-center font-Pretendard text-sm font-normal">
+          <span className="mr-3">이미 가입하셨나요?</span>{" "}
+          <Link
+            href="/signin"
+            className="text-[#5534Da]"
+            style={{ textDecoration: "underline" }}
+          >
+            로그인하기
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default signup;
+export default Signin;
