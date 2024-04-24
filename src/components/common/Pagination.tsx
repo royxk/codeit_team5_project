caebec979e094e8e30171842609a8e6712d302dc
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-24 21:30:12 +0900

    ✨ 페이지네이션에 페이지 1페이지로 갈 수 있도록 초기화 하는 기능 추가

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -6,12 +6,14 @@ interface PaginationProps {
   count: number;
   setCurrentPageData: (currentPageData: number) => void;
   pageItemLimit?: number;
+  pageRefreshSwitch?: boolean;
 }
 
 /**
  * @param {pageData[]} rawPageData 페이지에 표시할 순수 배열 데이터를 받는 param입니다. 이 데이터를 가공하여 [페이지][페이지 데이터] 형태의 2차원 배열로 가공합니다.
  * @param {StateSetFunction} setCurrentPageData 현재 페이지에 보여줄 데이터를 설정하는 setState함수를 받아 페이지 선택 시 그 값으로 설정합니다.
  * @param {number} pageItemLimit 현재 페이지에 얼마나 많은 개수를 표기할 지 선택하는 인수입니다. 기본적으로 코드잇에서 제공하는 5값으로 되어있으며 필요시 추가로 수정할 수 있게 하였습니다.
+ * @param {boolean} pageRefreshSwitch boolean 형이 변화할 때 마다 바뀌었다는 것을 알려주는 인자입니다. setState(!state) 형식으로 초기화가 필요한 작업 끝단에 넣어주시면 될 듯 합니다.
  * @returns
  */
 const Pagination = ({
@@ -20,6 +22,7 @@ const Pagination = ({
     console.log(`${item}`);
   },
   pageItemLimit = 5,
+  pageRefreshSwitch,
 }: PaginationProps) => {
   const pageData = Math.ceil(count / pageItemLimit);
   const [currentPage, setCurrentPage] = useState(0);
@@ -98,6 +101,10 @@ const Pagination = ({
     setCurrentPageList(firstPageList);
     handlePageNumberChange(0);
   }, []);
+
+  useEffect(() => {
+    handlePageNumberChange(0);
+  }, [pageRefreshSwitch]);
 
   return (
     <div className="flex h-16 w-full items-center justify-center gap-5 bg-white p-3 mob:h-[3.625rem]">
