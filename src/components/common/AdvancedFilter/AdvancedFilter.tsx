30830f40669a50c2a779ad25dfaba4615749429f
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-04-24 18:53:12 +0900

    ✨ Add form on Advanced Filter

--- src/components/common/AdvancedFilter/AdvancedFilter.tsx
+++ src/components/common/AdvancedFilter/AdvancedFilter.tsx
@@ -3,6 +3,7 @@ import LocationBox from "./LocationBox";
 import Button from "../Button";
 import SelectedLocationBox from "./SelectedLocationBox";
 import SvgCloseButton from "./SvgCloseButton";
+import DatePicker from "react-datepicker";
 
 interface AdvancedFilterProps {
   onClick: (event: React.MouseEvent) => void;
@@ -33,6 +34,11 @@ function AdvancedFilter({
     setcount && setcount(newCount);
   };
 
+  const handleFomrSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    handleSubmit();
+  };
+
   // Effect to handle inputs and locations changes
   useEffect(() => {
     updateCount();
@@ -50,6 +56,11 @@ function AdvancedFilter({
   };
 
   const handleSubmit = (): void => {
+    if (Number(price) < 0) {
+      alert("금액은 0 이상의 숫자만 입력 가능합니다.");
+      return;
+    }
+
     const filterData = {
       locations: locations,
       startDate: startDate,
@@ -74,57 +85,62 @@ function AdvancedFilter({
           <SvgCloseButton />
         </div>
       </div>
-      <div className={`flex flex-col gap-2`}>
-        <div>위치</div>
-        <LocationBox selectedLocations={locations} handleClick={addLocation} />
-        <SelectedLocationBox
-          selectedLocations={locations}
-          handleClick={deleteLocation}
-        />
-      </div>
-      <div className={`flex flex-col gap-2 border-t-2 py-4`}>
-        <div>시작일</div>
-        <input
-          className={`h-16 w-full rounded-xl border px-4 focus:outline-none`}
-          placeholder="입력"
-          type="date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-        ></input>
-      </div>
-      <div className={`flex w-full flex-col gap-3 border-t-2 py-4`}>
-        <div>금액</div>
-        <div className={`flex w-full flex-row items-center gap-2`}>
-          <div
-            className={`flex h-16 flex-row items-center gap-2 rounded-xl border px-4 `}
-          >
-            <input
-              className={`focus:outline-none`}
-              placeholder="입력"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-            <div>원</div>
-          </div>
-          <div>이상부터</div>
+      <form onSubmit={handleFomrSubmit}>
+        <div className={`flex flex-col gap-2`}>
+          <div>위치</div>
+          <LocationBox
+            selectedLocations={locations}
+            handleClick={addLocation}
+          />
+          <SelectedLocationBox
+            selectedLocations={locations}
+            handleClick={deleteLocation}
+          />
         </div>
-      </div>
-      <div
-        className={`flex w-full flex-row justify-between gap-3 mob:sticky mob:bottom-4 mob:border-t-2 mob:bg-white mob:pt-5`}
-      >
-        <Button
-          className={`w-[82px]`}
-          color="white"
-          size="full"
-          onClick={handleReset}
+        <div className={`flex flex-col gap-2 border-t-2 py-4`}>
+          <div>시작일</div>
+          <input
+            className={`h-16 w-full rounded-xl border px-4 focus:outline-none`}
+            placeholder="입력"
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          ></input>
+        </div>
+        <div className={`flex w-full flex-col gap-3 border-t-2 py-4`}>
+          <div>금액</div>
+          <div className={`flex w-full flex-row items-center gap-2`}>
+            <div
+              className={`flex h-16 flex-row items-center gap-2 rounded-xl border px-4 `}
+            >
+              <input
+                className={`focus:outline-none`}
+                placeholder="입력"
+                type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <div>원</div>
+            </div>
+            <div>이상부터</div>
+          </div>
+        </div>
+        <div
+          className={`flex w-full flex-row justify-between gap-3 mob:sticky mob:bottom-4 mob:border-t-2 mob:bg-white mob:pt-5`}
         >
-          초기화
-        </Button>
-        <Button color="red" size="full" onClick={handleSubmit}>
-          적용하기
-        </Button>
-      </div>
+          <Button
+            className={`w-[82px]`}
+            color="white"
+            size="full"
+            onClick={handleReset}
+          >
+            초기화
+          </Button>
+          <Button color="red" size="full" onClick={handleSubmit}>
+            적용하기
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
