abf6bea5f74e9f6d4b72726b21442c2380ea04d6
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-04-25 23:57:08 +0900

    ✨ notification modal mock 데이터로 테스트 완료

--- src/components/common/NotificationModal/NotificationModal.tsx
+++ src/components/common/NotificationModal/NotificationModal.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import Data from "@/util/constants/NOTIFICATION_MODAL_MOCK_DATA";
 import NotificationModalMessageBox from "./NotificationModalMessageBox";
 import SvgCloseComponent from "./SvgCloseComponent";
+import { NOTIFICATION_API_RESPONSE_TYPE } from "./NOTIFICATION_API_RESPONSE_TYPE";
 
 interface NotificationModalProps {
+  data: NOTIFICATION_API_RESPONSE_TYPE;
   onClick: (event: React.MouseEvent) => void;
 }
 
-const NotificationModal = ({ onClick }: NotificationModalProps) => {
+const NotificationModal = ({ onClick, data }: NotificationModalProps) => {
+  const contents = data.items;
+  console.log(contents[0].item.result);
   return (
     <div
       className={`flex min-h-96 w-96 flex-col gap-5 rounded-xl border border-gray-40  bg-red-10 px-5 py-6 mob:h-screen mob:w-full mob:rounded-none  mob:border-0 mob:pt-10`}
     >
       <div className={`flex flex-row justify-between`}>
-        <div className={`text-xl font-bold `}>알림 6개</div>
+        <div className={`text-xl font-bold `}>알림 {contents.length}개</div>
         <div
           className={`hidden select-none mob:inline-block`}
           onClick={onClick}
@@ -25,15 +29,8 @@ const NotificationModal = ({ onClick }: NotificationModalProps) => {
       <div
         className={`flex max-h-96 flex-col gap-2 overflow-y-auto mob:max-h-full`}
       >
-        {Data.map((data, index) => {
-          return (
-            <NotificationModalMessageBox
-              state={data.state}
-              message={data.message}
-              time={data.time}
-              key={index}
-            />
-          );
+        {contents.map((data) => {
+          return <NotificationModalMessageBox key={data.item.id} data={data} />;
         })}
       </div>
     </div>
