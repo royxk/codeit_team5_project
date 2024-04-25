85097bdcdbe16ddd06fa9a48698ad70e582b013d
seoyeon039 <ysynop7@naver.com>
2024-04-25 15:02:23 +0900

    💄 주석 추가

--- src/components/common/EmployerTable.tsx
+++ src/components/common/EmployerTable.tsx
@@ -1,8 +1,21 @@
 import { EmployerTableData, convertEmployerTableData } from "@/util/convertData";
 import { USER_API_RESPONSE } from "@/util/constants/table_mock_data";
 import Table from "./Table";
+import { getServerSideCookie } from "@/app/utils/serverCookies";
+import { searchSelectedNoticeApplyApiResponse } from "@/util/api";
 
 const EMPLOYER_TABLE_HEADER = ['신청자', '소개', '전화번호', '상태'];
+
+// 공고 상세 페이지 개설 시 할 예정
+// const getServerSideData = async () => {
+//   const sid = getServerSideCookie("sid");
+//   const noticeId = '';
+//   if (sid) {
+//     const applyData = await searchSelectedNoticeApplyApiResponse(sid, noticeId);
+//     return applyData;
+//   }
+//   return;
+// }
 
 const EmployerTable = () => {
   const employerData: EmployerTableData[] = convertEmployerTableData(USER_API_RESPONSE);
