4c516f3428a2d9150b6ea198a0fe65d750450f16
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-25 13:09:40 +0900

    ♻️  코드 로직 변경

--- src/components/home/NoticeMain.tsx
+++ src/components/home/NoticeMain.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import BasicFilter from "./BasicFilter";
 import AdvancedFilterComponent from "../common/AdvancedFilter/AdvancedFilterComponent";
 import FilterdNotice from "./FilterdNotice";
@@ -10,6 +10,7 @@ import {
 } from "@/util/convertData";
 import type { NoticeResponse } from "@/app/page";
 import { searchNoticeApiResponse } from "@/util/api";
+import { BASIC_FILTER_INITIALVALUE } from "@/util/constants/FILTER_VALUES";
 
 type Props = {};
 
@@ -19,25 +20,40 @@ const NoticeMain = (props: Props) => {
   const [sortedQuery, setSortedQuery] = useState<ConvertedSortType | null>(
     null,
   );
-  const [prevSortedQuery, setPrevSortedQuery] =
-    useState<ConvertedSortType | null>(null);
-  const [isSortedQueryChanged, setIsSortedQueryChanged] = useState(false);
+  const [isFilterChanged, setIsFilterChanged] = useState(false);
+  const [filterSelected, setFilterSelected] = useState<SortType>(
+    BASIC_FILTER_INITIALVALUE,
+  );
+  const [prevFilterSelected, setPrevFilterSelected] = useState<SortType>(
+    BASIC_FILTER_INITIALVALUE,
+  );
+  const handleFilterSelectedClick = (e: React.MouseEvent) => {
+    setFilterSelected((e.target as HTMLButtonElement).innerHTML as SortType);
+    setPrevFilterSelected(filterSelected);
+  };
   const handleBasicFilterClick = async (sortType: SortType) => {
     const sortQuery = convertSortType(sortType);
+    setSortedQuery(sortQuery);
     const res = await searchNoticeApiResponse({ limit: 6, sort: sortQuery });
     console.log(res);
     setFilterdByBasicNoticeList(res);
-
-    setSortedQuery(sortQuery);
   };
+
   useEffect(() => {
-    if (sortedQuery !== prevSortedQuery) {
-      setIsSortedQueryChanged(true);
-      setPrevSortedQuery(sortedQuery);
-    } else {
-      setIsSortedQueryChanged(false);
-    }
-  }, [sortedQuery, prevSortedQuery]);
+    const executeFilter = async () => {
+      console.log(filterSelected);
+      console.log(prevFilterSelected);
+      if (filterSelected !== prevFilterSelected) {
+        setIsFilterChanged(true);
+        await handleBasicFilterClick(filterSelected);
+      } else {
+        setIsFilterChanged(false);
+      }
+    };
+
+    executeFilter();
+  }, [filterSelected, prevFilterSelected]);
+
   return (
     <section className="flex justify-center py-[60px]">
       <div className=" mx-auto flex w-[964px] flex-col items-center">
@@ -47,12 +63,16 @@ const NoticeMain = (props: Props) => {
         >
           <h1 className="text-[28px] font-bold mob:text-xl">전체공고</h1>
           <div className="flex h-[42px] items-center gap-[10px]">
-            <BasicFilter onBasicFilterClick={handleBasicFilterClick} />
+            <BasicFilter
+              filterSelected={filterSelected}
+              onFilterSelectedClick={handleFilterSelectedClick}
+            />
             <AdvancedFilterComponent />
           </div>
         </div>
         <FilterdNotice
-          isSortedQueryChanged={isSortedQueryChanged}
+          setIsFilterChanged={setIsFilterChanged}
+          isFilterChanged={isFilterChanged}
           sortedQuery={sortedQuery}
           basicNoticeList={filterdByBasicNoticeList}
         />
