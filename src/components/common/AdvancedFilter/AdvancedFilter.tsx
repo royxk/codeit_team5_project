86171179465ae4da96bbac62cb714935c7c88c2f
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-26 23:51:33 +0900

    ✨ 오늘 날짜 이전은 선택하지 못하도록 필터를 설정, 페이징 눌렀을 때 선택한 시간이 있다면 현재시간을 포함하는 날짜를 RFC3339로 변환한 시간으로 설정해 api 요청

--- src/components/common/AdvancedFilter/AdvancedFilter.tsx
+++ src/components/common/AdvancedFilter/AdvancedFilter.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import LocationBox from "./LocationBox";
 import Button from "../Button";
 import SelectedLocationBox from "./SelectedLocationBox";
 import SvgCloseButton from "./SvgCloseButton";
-import { formatDateToRFC3339 } from "@/util/formatDate";
+import { getCurrentDate, getCurrentRFC3339DateTime } from "@/util/formatDate";
 import type { AdvancedFilterQuery } from "@/util/convertData";
 import { Address } from "@/util/api";
 import Link from "next/link";
@@ -60,11 +61,12 @@ function AdvancedFilter({
       query.address = locations as Address[];
     }
     if (startDate) {
-      query.startsAtGte = formatDateToRFC3339(startDate);
+      query.startsAtGte = getCurrentRFC3339DateTime();
     }
     if (price) {
       query.hourlyPayGte = Number(price);
     }
+    console.log(startDate);
     console.log(query);
     onAdvencedFilterSubmit(query);
     setIsAdvancedFilterOpen(false);
@@ -76,73 +78,76 @@ function AdvancedFilter({
   }, [locations, startDate, price]);
 
   return (
-    <div
-      className={`z-50 flex w-96 flex-col justify-between gap-4 rounded-xl border-2 bg-white px-5 py-6 mob:h-screen mob:w-full mob:rounded-none mob:border-none`}
-    >
-      <div className={`flex flex-row justify-between`}>
-        <div className={`text-[20px] font-bold`}>상세필터</div>
-        <div onClick={onClick} className="hover:cursor-pointer">
-          <SvgCloseButton />
-        </div>
-      </div>
-      <div className={`flex flex-col gap-2`}>
-        <div>위치</div>
-        <LocationBox selectedLocations={locations} onClick={addLocation} />
-        <SelectedLocationBox
-          selectedLocations={locations}
-          onClick={deleteLocation}
-        />
-      </div>
-      <div className={`flex flex-col gap-2 border-t-2 py-4`}>
-        <div>시작일</div>
-        <input
-          className={`h-16 w-full rounded-xl border px-4 focus:outline-none`}
-          placeholder="입력"
-          type="date"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
-        ></input>
-      </div>
-      <div className={`flex w-full flex-col gap-3 border-t-2 py-4`}>
-        <div>금액</div>
-        <div className={`flex w-full flex-row items-center gap-2`}>
-          <div
-            className={`flex h-16 flex-row items-center gap-2 rounded-xl border px-4 `}
-          >
-            <input
-              className={`focus:outline-none`}
-              placeholder="입력"
-              type="number"
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
-            <div>원</div>
-          </div>
-          <div>이상부터</div>
-        </div>
-      </div>
+    <>
       <div
-        className={`flex w-full flex-row justify-between gap-3 mob:sticky mob:bottom-4 mob:border-t-2 mob:bg-white mob:pt-5`}
+        className={`z-50 flex w-96 flex-col justify-between gap-4 rounded-xl border-2 bg-white px-5 py-6 mob:h-screen mob:w-full mob:rounded-none mob:border-none`}
       >
-        <Button
-          className={`w-[82px]`}
-          color="white"
-          size="full"
-          onClick={handleReset}
+        <div className={`flex flex-row justify-between`}>
+          <div className={`text-[20px] font-bold`}>상세필터</div>
+          <div onClick={onClick} className="hover:cursor-pointer">
+            <SvgCloseButton />
+          </div>
+        </div>
+        <div className={`flex flex-col gap-2`}>
+          <div>위치</div>
+          <LocationBox selectedLocations={locations} onClick={addLocation} />
+          <SelectedLocationBox
+            selectedLocations={locations}
+            onClick={deleteLocation}
+          />
+        </div>
+        <div className={`flex flex-col gap-2 border-t-2 py-4`}>
+          <div>시작일</div>
+          <input
+            className={`h-16 w-full rounded-xl border px-4 focus:outline-none`}
+            placeholder="입력"
+            min={getCurrentDate()}
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+          ></input>
+        </div>
+        <div className={`flex w-full flex-col gap-3 border-t-2 py-4`}>
+          <div>금액</div>
+          <div className={`flex w-full flex-row items-center gap-2`}>
+            <div
+              className={`flex h-16 flex-row items-center gap-2 rounded-xl border px-4 `}
+            >
+              <input
+                className={`focus:outline-none`}
+                placeholder="입력"
+                type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <div>원</div>
+            </div>
+            <div>이상부터</div>
+          </div>
+        </div>
+        <div
+          className={`flex w-full flex-row justify-between gap-3 mob:sticky mob:bottom-4 mob:border-t-2 mob:bg-white mob:pt-5`}
         >
-          초기화
-        </Button>
-        <Link
-          className="w-full"
-          onClick={handleSubmit}
-          href={"#filterdNoticeSection"}
-        >
-          <Button color="red" size="full">
-            적용하기
+          <Button
+            className={`w-[82px]`}
+            color="white"
+            size="full"
+            onClick={handleReset}
+          >
+            초기화
           </Button>
-        </Link>
+          <Link
+            className="w-full"
+            onClick={handleSubmit}
+            href={"#filterdNoticeSection"}
+          >
+            <Button color="red" size="full">
+              적용하기
+            </Button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
