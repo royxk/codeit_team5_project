48b8531f59d55c180c13315b45c34725fdf75aa5
SoohwanKim <155133655+soohwan93@users.noreply.github.com>
2024-04-26 11:49:22 +0900

    ♻️ 불필요한 코드 제거

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import NoticeMain from "@/components/home/NoticeMain";
 import { useSearchParams } from "next/navigation";
-import React from "react";
 
 const SearchPage = () => {
   const searchParams = useSearchParams();
   const keyword = searchParams.get("keyword");
+
   return <NoticeMain keyword={keyword} />;
 };
 
