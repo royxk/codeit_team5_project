8e6e098c9a2c6582623bfbda9f5cfaaf2caac186
Rhajiit <ksd9638@gmail.com>
2024-04-26 12:51:40 +0900

    💄 사전 작성된 컴포넌트 배치

--- src/app/employer/notice/[noticeId]/page.tsx
+++ src/app/employer/notice/[noticeId]/page.tsx
@@ -1,5 +1,17 @@
+import EmployerTable from "@/components/common/EmployerTable";
+import StoreDetail from "@/components/common/StoreDetail";
+
 const EmployerNoticeDetailPage = () => {
-  return <div className="min-h-[calc(100vh-170px)] bg-gray-5"></div>;
+  return (
+    <div className="min-h-[calc(100vh-170px)] bg-gray-5">
+      <div className="mx-auto max-w-[64.25rem] px-8 py-[3.75rem]">
+        <StoreDetail />
+      </div>
+      <div className="mx-auto max-w-[64.25rem] px-8 py-[3.75rem]">
+        <EmployerTable />
+      </div>
+    </div>
+  );
 };
 
 export default EmployerNoticeDetailPage;
