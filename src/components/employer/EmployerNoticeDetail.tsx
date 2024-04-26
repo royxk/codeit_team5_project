8d6a71580c816ec1b66465a9a22b70bd495a53de
Rhajiit <ksd9638@gmail.com>
2024-04-26 13:11:51 +0900

    ✨ 사장이 공고 페이지에 접근 하였을 때 공고를 불러오도록 설정

--- src/components/employer/EmployerNoticeDetail.tsx
+++ src/components/employer/EmployerNoticeDetail.tsx
@@ -1,15 +1,32 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EmployerTable from "@/components/common/EmployerTable";
 import StoreDetail from "@/components/common/StoreDetail";
+import { searchSelectedNoticeApiResponse } from "@/util/api";
+import { getCookie } from "@/util/cookieSetting";
+import useNoticeId from "./Hook/useNoticeId";
 
 type Props = {};
 
-const EmployerNoticeDetail = (props: Props) => {
+const EmployerNoticeDetail = () => {
+  const [noticeData, setNoticeData] = useState();
+
+  const shopId = getCookie("sid")!;
+  const noticeId = useNoticeId()!;
+
+  async function handleNoticeData() {
+    const noticeData = await searchSelectedNoticeApiResponse(shopId, noticeId);
+    setNoticeData(noticeData);
+  }
+
+  useEffect(() => {
+    handleNoticeData();
+  }, []);
+
   return (
     <>
       <div className="mx-auto max-w-[64.25rem] px-8 py-[3.75rem]">
-        <StoreDetail />
+        <StoreDetail data={noticeData} />
       </div>
       <div className="mx-auto max-w-[64.25rem] px-8 py-[3.75rem]">
         <EmployerTable />
