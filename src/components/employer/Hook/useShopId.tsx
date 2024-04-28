a8ffb45ae31f346fcc13a2706e3b245ba336d12c
Rhajiit <ksd9638@gmail.com>
2024-04-28 14:02:02 +0900

    ♻️ useShopId를 조금 더 수정

--- src/components/employer/Hook/useShopId.tsx
+++ src/components/employer/Hook/useShopId.tsx
@@ -1,9 +1,10 @@
+import { getCookie } from "@/util/cookieSetting";
 import { useParams } from "next/navigation";
 
 const useShopId = () => {
   const currentUrl = useParams<{ shopId: string }>();
 
-  return currentUrl.shopId;
+  return currentUrl?.shopId || getCookie("sid");
 };
 
 export default useShopId;
