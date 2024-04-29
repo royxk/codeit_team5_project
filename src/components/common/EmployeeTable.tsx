f8e9914cb4965f441b9c597e62401b4098f50996
Rhajiit <ksd9638@gmail.com>
2024-04-29 11:57:53 +0900

    🐛 고용자가 최근 신청한 10개의 내역만 볼 수 있는 문제 수정

--- src/components/common/EmployeeTable.tsx
+++ src/components/common/EmployeeTable.tsx
@@ -1,5 +1,9 @@
-'use client';
-import { ApplyListApiResponse, EmployeeTableData, convertEmployeeTableData } from "@/util/convertData";
+"use client";
+import {
+  ApplyListApiResponse,
+  EmployeeTableData,
+  convertEmployeeTableData,
+} from "@/util/convertData";
 import { searchUserApplyApiResponse } from "@/util/api";
 import { useEffect, useState } from "react";
 import { getCookie } from "@/util/cookieSetting";
@@ -7,7 +11,7 @@ import Table from "./Table";
 import Button from "./Button";
 import Link from "next/link";
 
-const EMPLOYEE_TABLE_HEADER = ['가게', '일자', '시급', '상태'];
+const EMPLOYEE_TABLE_HEADER = ["가게", "일자", "시급", "상태"];
 
 const EmployeeTable = () => {
   const [applyData, setApplyData] = useState<ApplyListApiResponse>();
@@ -17,7 +21,10 @@ const EmployeeTable = () => {
 
   async function getApplyData(userId: string | undefined) {
     if (!userId) return;
-    const data = await searchUserApplyApiResponse(userId);
+    const countData = await searchUserApplyApiResponse(userId, { limit: 1 });
+    const data = await searchUserApplyApiResponse(userId, {
+      limit: countData.count,
+    });
     return data;
   }
 
@@ -27,18 +34,27 @@ const EmployeeTable = () => {
       setApplyData(data);
     };
     fetchUserData();
-  }, [userId])
+  }, [userId]);
 
-  return (
-    employeeData.length ? <Table<EmployeeTableData> headerData={EMPLOYEE_TABLE_HEADER} applyData={employeeData} />
-    :
-    <div className="flex flex-col items-center gap-6 w-full max-w-[964px] border border-gray-20 rounded-lg px-6 py-[60px] mob:text-sm mob:gap-4">
+  return employeeData.length ? (
+    <Table<EmployeeTableData>
+      headerData={EMPLOYEE_TABLE_HEADER}
+      applyData={employeeData}
+    />
+  ) : (
+    <div className="flex w-full max-w-[964px] flex-col items-center gap-6 rounded-lg border border-gray-20 px-6 py-[60px] mob:gap-4 mob:text-sm">
       <p className="text-black">아직 신청 내역이 없어요.</p>
       <Link href="/">
-        <Button size="large" color="red" className="mob:px-5 mob:py-[10px] mob:text-sm">공고 보러가기</Button>
+        <Button
+          size="large"
+          color="red"
+          className="mob:px-5 mob:py-[10px] mob:text-sm"
+        >
+          공고 보러가기
+        </Button>
       </Link>
     </div>
   );
-}
+};
 
 export default EmployeeTable;
